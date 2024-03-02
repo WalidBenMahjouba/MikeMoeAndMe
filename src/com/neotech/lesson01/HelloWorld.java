1fe0098664a606381a3afeb33604797a39a0efda
WalidBen <walidbenmahjouba@gmail.com>
2024-03-02 17:08:08 -0500

    Added the first feature

---
+++ src/com/neotech/lesson01/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.neotech.lesson01;
+
+public class HelloWorld {
+//To Save hit CTRL+S
+	// I am telling Java ignore this -- this is just for my eyes
+	// this is the main method
+	public static void main (String[]args) {
+		// this method is a very special method
+		// shortcut for printLn:
+		//sysout then CTRL+SPACE
+		System.out.println("Hello World!");
+	}
+}
